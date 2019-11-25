f4af6221beae2247d2658c412e890315684a9dab
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2019-11-25 10:13:31 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  title: 'test',
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('Test')
+    ),
+    body: Center(
+      child: Text('Test')
+    )
+  )
+));
